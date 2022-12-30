2dcf3e606bed65c1ca6e9a076435f72c98e78004
Adrian Schneider <adrian.schneider@rgbi.ch>
2022-12-30 21:13:24 +0100

    Added settings menu

---
+++ Assets/Scripts/UI/Settings/SettingsSceneManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mixin.Utils;
+
+namespace Mixin.TheLastMove
+{
+    public class SettingsSceneManager : MonoBehaviour
+    {
+        private void Start()
+        {
+            
+        }
+
+        public void LoadSettings()
+        {
+
+        }
+
+        private void SaveSettings()
+        {
+
+        }
+    }
+}
