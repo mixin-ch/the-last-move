91464181889f3200f997f52e4ec1b4d97e256351
Adrian Schneider <adrian.schneider@rgbi.ch>
2023-01-06 18:18:39 +0100

    Added new elements to about page

--- Assets/Scripts/About/AboutUIB.cs
+++ Assets/Scripts/About/AboutUIB.cs
@@ -6,13 +6,23 @@ namespace Mixin.TheLastMove.About
 {
     public class AboutUIB : UIBuildManager<AboutUIB>
     {
-        public Button ExitButton;
+        public Button ExitButton { get; set; }
+        public Label AboutText { get; set; }
+        public Button ProjectWebsiteButton { get; set; }
+        public Button redgreenbirdFilmsLogo { get; set; }
+        public Button MixinGamesLogo { get; set; }
+        public Button PrivacyButton { get; set; }
 
         protected override void Awake()
         {
             base.Awake();
 
             ExitButton = _root.Q<Button>("ExitButton");
+            AboutText = _root.Q<Label>("ExitButton");
+            ProjectWebsiteButton = _root.Q<Button>("ProjectWebsiteButton");
+            redgreenbirdFilmsLogo = _root.Q<Button>("redgreenbirdFilmsLogo");
+            MixinGamesLogo = _root.Q<Button>("MixinGamesLogo");
+            PrivacyButton = _root.Q<Button>("PrivacyButton");
         }
     }
 }
