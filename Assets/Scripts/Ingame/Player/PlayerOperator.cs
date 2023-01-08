5f3d07602423596d3122d4f1cfc08d6ee20e5a01
Nixtrin <mischa.iwasaki@gmail.com>
2023-01-08 12:34:05 +0100

    Stretching and Jumping

--- Assets/Scripts/Ingame/Player/PlayerOperator.cs
+++ Assets/Scripts/Ingame/Player/PlayerOperator.cs
@@ -1,5 +1,6 @@
 using Mixin.Utils;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Mixin.TheLastMove
@@ -13,55 +14,66 @@ namespace Mixin.TheLastMove
         private Rigidbody2D _rigidbody;
         [SerializeField]
         private Transform _imageTransform;
+        [SerializeField]
+        private Transform _stretcherTransform;
+        [SerializeField]
+        private Collider2D _collider;
+
+        private float _stretchCatchup = 300f;
+        private float _stretchCushion = 10f;
+        private float _stretchExtent = 1f;
 
         [SerializeField]
         private Vector2 _startPosition = Vector2.up * 3;
         [SerializeField]
-        private int _jumps = 2;
+        private List<float> _jumpList;
         [SerializeField]
         private int _startHealth = 1;
-        private float _gravity = 8f;
-        private float _jumpVelocity = 3f;
-        private float _jumpTime = 0.5f;
+        private float _gravity = 1f;
+        private float _jumpVelocityBreak = 5f;
 
         private float Hectic => EnvironmentManager.Instance.Hectic;
 
         private float Gravity => _gravity * Hectic;
-        private float JumpVelocity => _jumpVelocity * Mathf.Sqrt(Hectic);
-        private int Jumps => _jumps;
-        private float JumpTime => _jumpTime / Mathf.Sqrt(Hectic);
 
         private float _health;
-        private float _velocity;
-        private int _remainingJumps;
-        private bool _isJumping;
-        private float _jumpTimeRemaining;
 
-        private bool HasJump => _remainingJumps > 0;
+        private float _pausedVelocity;
+
+        private float _dragPointOffset;
+        private float _dragPointVelocity;
+
+        private List<float> _remainingJumpList = new List<float>();
+        private bool _isJumping;
+
+        private bool HasJump => _remainingJumpList.Count > 0;
 
         public Vector2 Position => transform.position;
         public float Health { get => _health; }
-
 
         public void Tick(float time)
         {
             _isJumping = _isJumping && InputManager.Instance.IsPressingJumpButton;
 
-            if (!_isJumping)
-                _jumpTimeRemaining = 0;
-            else
-                _jumpTimeRemaining = (_jumpTimeRemaining - time).LowerBound(0);
+            if (!_isJumping && _rigidbody.velocity.y > 0)
+                _rigidbody.velocity = Vector2.up * Mathf.Lerp(_rigidbody.velocity.y, 0, _jumpVelocityBreak * time);
 
-            if (_jumpTimeRemaining <= 0)
-            {
-                _isJumping = false;
-                _velocity += -Gravity * time;
-            }
+            _rigidbody.gravityScale = Gravity;
 
-            RefreshVelocity();
+            ProcessStretch(time);
 
             if (Position.y < -5)
                 Die();
+        }
+
+        private void ProcessStretch(float time)
+        {
+            _dragPointOffset = _dragPointOffset + (_dragPointVelocity - _rigidbody.velocity.y) * time;
+            _dragPointOffset = Mathf.Lerp(_dragPointOffset, 0, _stretchCushion * time);
+            _dragPointVelocity -= _dragPointOffset * _stretchCatchup * time;
+
+            float stretch = (_dragPointOffset * _stretchExtent).AmplifierToMultiplier();
+            _stretcherTransform.localScale = new Vector3(1 / stretch, stretch, 1);
         }
 
         public void PauseRefresh()
@@ -77,10 +89,8 @@ namespace Mixin.TheLastMove
                 return;
 
             _isJumping = true;
-            _remainingJumps--;
-            _jumpTimeRemaining = JumpTime;
-            _velocity = JumpVelocity;
-            RefreshVelocity();
+            _rigidbody.velocity = Vector2.up * _remainingJumpList[0] * Mathf.Sqrt(Hectic);
+            _remainingJumpList.RemoveAt(0);
         }
 
         private void TakeDamage()
@@ -101,33 +111,38 @@ namespace Mixin.TheLastMove
         public void ResetState()
         {
             transform.position = _startPosition;
+            _rigidbody.gravityScale = 0;
             _rigidbody.velocity = Vector2.zero;
             _imageTransform.localScale = Vector2.one;
 
             _health = _startHealth;
-            _velocity = 0;
-            _remainingJumps = 0;
+            _pausedVelocity = 0;
+            _remainingJumpList.Clear();
             _isJumping = false;
-            _jumpTimeRemaining = 0;
+            _dragPointOffset = 1;
         }
 
         private void RefreshVelocity()
         {
             if (EnvironmentManager.Instance.Paused)
+            {
+                _pausedVelocity = _rigidbody.velocity.y;
                 _rigidbody.velocity = Vector2.zero;
+            }
             else
-                _rigidbody.velocity = Vector2.up * _velocity;
+            {
+                _rigidbody.velocity = Vector2.up * _pausedVelocity;
+                _pausedVelocity = 0;
+            }
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            if (collision.enabled && _velocity <= 0)
+            if (collision.enabled)
             {
-                _velocity = 0;
-                _jumpTimeRemaining = 0;
-                _remainingJumps = Jumps;
+                _remainingJumpList.Clear();
+                _remainingJumpList.AddRange(_jumpList);
                 _isJumping = false;
-                RefreshVelocity();
             }
         }
 
