f9c66b21725deb3a83d4d2fe07a44388195d5057
Adrian Schneider <adrian.schneider@rgbi.ch>
2023-01-12 17:22:53 +0100

    Added Play Games Manager

---
+++ Assets/Scripts/General/GooglePlayGamesManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+namespace Mixin.TheLastMove.Social
+{
+    public class GooglePlayGamesManager : MonoBehaviour
+    {
+        public void Start()
+        {
+            PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+        }
+
+        internal void ProcessAuthentication(SignInStatus status)
+        {
+            if (status == SignInStatus.Success)
+            {
+                // Continue with Play Games Services
+            }
+            else
+            {
+                // Disable your integration with Play Games Services or show a login button
+                // to ask users to sign-in. Clicking it should call
+                // PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication).
+            }
+        }
+    }
+}
+
