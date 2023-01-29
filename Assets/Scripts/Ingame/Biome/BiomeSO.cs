6e7a414d4159022f7bca3b44bb9f5f5d2e06bb78
Adrian Schneider <adrian.schneider@rgbi.ch>
2023-01-29 17:37:35 +0100

    Added fog color field

--- Assets/Scripts/Ingame/Biome/BiomeSO.cs
+++ Assets/Scripts/Ingame/Biome/BiomeSO.cs
@@ -8,6 +8,9 @@ namespace Mixin.TheLastMove.Environment
         [SerializeField]
         private Sprite _background;
 
+        [SerializeField]
+        private Color _fogColor = Color.white;
+
         [System.Obsolete]
         [SerializeField]
         private Sprite _sprite;
@@ -18,5 +21,6 @@ namespace Mixin.TheLastMove.Environment
         public Sprite Sprite { get => _sprite; }
         public Sprite Background { get => _background; }
         public GameObject Prefab { get => _prefab; }
+        public Color FogColor { get => _fogColor; }
     }
 }
