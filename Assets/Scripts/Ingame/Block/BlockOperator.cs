173c484694b06bfc4e5f4ed82c2fbb578968ba32
Adrian Schneider <adrian.schneider@rgbi.ch>
2023-01-31 08:30:52 +0100

    Added terrain decoration

--- Assets/Scripts/Ingame/Block/BlockOperator.cs
+++ Assets/Scripts/Ingame/Block/BlockOperator.cs
@@ -1,4 +1,5 @@
 using Mixin.TheLastMove.Environment;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Mixin.TheLastMove
@@ -15,23 +16,31 @@ namespace Mixin.TheLastMove
         [SerializeField]
         private SpriteRenderer _fundament;
 
+        [SerializeField]
+        private SpriteRenderer[] _decorationList;
+
         public Vector2 Position => transform.position;
 
         public SpriteRenderer MainSprite { get => _mainSprite; }
 
         public SpriteRenderer Fundament { get => _fundament; }
+        public SpriteRenderer[] DecorationList { get => _decorationList; }
 
         public void Setup(Vector3 position, float size, BiomeSO biome)
         {
+            BlockOperator prefab = biome.Prefab.GetComponent<BlockOperator>();
+
             transform.position = position;
             transform.localScale = Vector2.one * size;
 
             _mainSprite.sprite = biome.Sprite;
             _mainSprite.gameObject.transform.localPosition =
-                biome.Prefab.GetComponent<BlockOperator>().MainSprite.transform.localPosition;
+                prefab.MainSprite.transform.localPosition;
 
             _fundament.color =
-                 biome.Prefab.GetComponent<BlockOperator>()._fundament.color;
+                 prefab._fundament.color;
+
+            SetDecoration(prefab);
         }
 
         public void Move(Vector2 offset)
@@ -42,6 +51,18 @@ namespace Mixin.TheLastMove
         public void Destroy()
         {
             Destroy(gameObject);
+        }
+
+        private void SetDecoration(BlockOperator prefab)
+        {
+            for (int i = 0; i < _decorationList.Length; i++)
+            {
+                // Set position
+                _decorationList[i].transform.localPosition = prefab.DecorationList[i].transform.localPosition;
+
+                // Set object active or not
+                _decorationList[i].gameObject.SetActive(prefab.DecorationList[i].gameObject.activeSelf);
+            }
         }
     }
 }
