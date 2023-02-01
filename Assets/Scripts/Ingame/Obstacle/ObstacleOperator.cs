f50588c9da48ae34cc472062f5db6c3e669cf46d
Adrian Schneider <adrian.schneider@rgbi.ch>
2023-02-01 17:19:28 +0100

    Added obstacle death cloud

--- Assets/Scripts/Ingame/Obstacle/ObstacleOperator.cs
+++ Assets/Scripts/Ingame/Obstacle/ObstacleOperator.cs
@@ -1,10 +1,29 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Mixin.TheLastMove.Environment
 {
     public class ObstacleOperator : MonoBehaviour
     {
+        [SerializeField]
+        private SpriteRenderer _spriteRenderer;
+
+        [SerializeField]
+        private Sprite _sprite;
+
+        [SerializeField]
+        private Sprite _deathSprite;
+
+        [SerializeField]
+        private Collider2D _collider2D;
+
+        [SerializeField]
+        private float _fadeTime = 1f;
+
+        [SerializeField]
+        private float _scaleFactor = 1.2f;
+
         public static int Counter;
 
         public static event Action<ObstacleOperator> OnKilled;
@@ -16,7 +35,11 @@ namespace Mixin.TheLastMove.Environment
 
         public void Setup(Vector3 position)
         {
+            _spriteRenderer.sprite = _sprite;
+            _spriteRenderer.color = Color.white;
             transform.position = position;
+            _spriteRenderer.transform.localScale = new Vector3(1, 1, 1);
+            _collider2D.enabled = true;
         }
 
         public void Move(Vector2 offset)
@@ -26,7 +49,7 @@ namespace Mixin.TheLastMove.Environment
 
         public void Kill()
         {
-            MapManager.Instance.KillObstacle(this);
+            StartCoroutine(FadeAndScale());
             Counter++;
             OnKilled?.Invoke(this);
         }
@@ -34,6 +57,25 @@ namespace Mixin.TheLastMove.Environment
         private void ResetCounter()
         {
             Counter = 0;
+        }
+
+        private IEnumerator FadeAndScale()
+        {
+            float elapsedTime = 0f;
+            Color originalColor = _spriteRenderer.color;
+
+            _collider2D.enabled = false;
+            _spriteRenderer.sprite = _deathSprite;
+
+            while (elapsedTime < _fadeTime)
+            {
+                elapsedTime += Time.deltaTime;
+                _spriteRenderer.color = Color.Lerp(originalColor, Color.clear, elapsedTime / _fadeTime);
+                _spriteRenderer.transform.localScale *= _scaleFactor;
+                yield return null;
+            }
+
+            MapManager.Instance.KillObstacle(this);
         }
 
         public Vector2 Position => transform.position;
