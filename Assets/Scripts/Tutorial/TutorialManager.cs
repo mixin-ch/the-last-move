19ba07e385ab837d223a76d9c6ca30cf5e84fbc3
Paulli <61700276+Paulli4242@users.noreply.github.com>
2023-07-17 23:00:22 +0200

    Tutorial created TutorialManager

---
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ShowNextPage()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
